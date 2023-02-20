1229d79dfa0e7e03ccd30366081796a56d76ea0e
AlenaNovik <elena.pigaleva@gmail.com>
2023-02-20 21:38:54 +0300

    fix:migrations

--- src/config/ormconfig-migrations.ts
+++ src/config/ormconfig-migrations.ts
@@ -1,4 +1,8 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
 import { typeOrmConfig } from './ormconfig';
 
-export default new DataSource(typeOrmConfig);
+export default new DataSource({
+  ...typeOrmConfig,
+  url: process.env.DATABASE_MIGRATIONS,
+} as DataSourceOptions);
